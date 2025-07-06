015fc89bc8ef2222ead125cb6a28c626467a65d5
Brijesh <programmercode102@gmail.com>
2025-07-06 09:26:01 +0530

    added render-build.sh for libreoffice install

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ from pdf2docx import Converter
 import uuid
 from docx2pdf import convert as docx_to_pdf_convert
 from fastapi.middleware.cors import CORSMiddleware
+import subprocess
+from fastapi import HTTPException
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173" , "https://pdf-word-convertor-frontend.vercel.app"],  # or ["*"] for all
+    allow_origins=["http://localhost:5173" , "https://pdf-word-convertor-frontend.vercel.app" ],  # or ["*"] for all
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -31,15 +33,39 @@ async def pdf_to_word(file: UploadFile = File(...)):
     return FileResponse(output_path, media_type="application/vnd.openxmlformats-officedocument.wordprocessingml.document", filename="converted.docx")
 
 
+# @app.post("/word-to-pdf/")
+# async def word_to_pdf(file: UploadFile = File(...)):
+#     input_path = os.path.join(TEMP_DIR, f"{uuid.uuid4()}.docx")
+#     output_path = input_path.replace(".docx", ".pdf")
+
+#     with open(input_path, "wb") as f:
+#         f.write(await file.read())
+
+#     # Convert .docx to .pdf
+#     docx_to_pdf_convert(input_path, output_path)
+
+#     return FileResponse(output_path, media_type="application/pdf", filename="converted.pdf")
+
+
 @app.post("/word-to-pdf/")
 async def word_to_pdf(file: UploadFile = File(...)):
     input_path = os.path.join(TEMP_DIR, f"{uuid.uuid4()}.docx")
-    output_path = input_path.replace(".docx", ".pdf")
+    output_dir = TEMP_DIR
 
     with open(input_path, "wb") as f:
         f.write(await file.read())
 
-    # Convert .docx to .pdf
-    docx_to_pdf_convert(input_path, output_path)
+    try:
+        subprocess.run([
+            "libreoffice",
+            "--headless",
+            "--convert-to", "pdf",
+            "--outdir", output_dir,
+            input_path
+        ], check=True)
+    except subprocess.CalledProcessError as e:
+        raise HTTPException(status_code=500, detail=f"LibreOffice conversion failed: {e}")
+
+    output_path = input_path.replace(".docx", ".pdf")
 
     return FileResponse(output_path, media_type="application/pdf", filename="converted.pdf")
