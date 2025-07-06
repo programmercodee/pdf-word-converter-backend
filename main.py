894badc62a3c2bca7015458d13abc9375a13bba2
Brijesh <programmercode102@gmail.com>
2025-07-06 08:52:06 +0530

    fisrt commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import FileResponse
+import os
+from pdf2docx import Converter
+import uuid
+from docx2pdf import convert as docx_to_pdf_convert
+from fastapi.middleware.cors import CORSMiddleware
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173" , "https://pdf-word-convertor-frontend.vercel.app"],  # or ["*"] for all
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+TEMP_DIR = "temp"
+os.makedirs(TEMP_DIR, exist_ok=True)
+
+@app.post("/pdf-to-word/")
+async def pdf_to_word(file: UploadFile = File(...)):
+    input_path = os.path.join(TEMP_DIR, f"{uuid.uuid4()}.pdf")
+    output_path = input_path.replace(".pdf", ".docx")
+
+    with open(input_path, "wb") as f:
+        f.write(await file.read())
+
+    cv = Converter(input_path)
+    cv.convert(output_path)
+    cv.close()
+
+    return FileResponse(output_path, media_type="application/vnd.openxmlformats-officedocument.wordprocessingml.document", filename="converted.docx")
+
+
+@app.post("/word-to-pdf/")
+async def word_to_pdf(file: UploadFile = File(...)):
+    input_path = os.path.join(TEMP_DIR, f"{uuid.uuid4()}.docx")
+    output_path = input_path.replace(".docx", ".pdf")
+
+    with open(input_path, "wb") as f:
+        f.write(await file.read())
+
+    # Convert .docx to .pdf
+    docx_to_pdf_convert(input_path, output_path)
+
+    return FileResponse(output_path, media_type="application/pdf", filename="converted.pdf")
